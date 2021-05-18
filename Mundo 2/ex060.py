155fb6f0a09311e08f8d57c2b4d2995dea84ae8e
Vitor Neves Góes <77070562+torvigoes@users.noreply.github.com>
2021-05-18 16:32:09 -0300

    changes

--- Mundo 2/ex060.py
+++ Mundo 2/ex060.py
@@ -1,14 +1,15 @@
 #  CÁLCULO FATORIAL
 
-n = int(input('Digite um número para saber seu fatorial: '))
 
-count = n  # Contador começará com o valor igual o de n, para que na hora de printar o fatorial saia correto
+n = int(input('Digite um número para o cálculo fatorial: ').strip())
+
+count = n  # Para que o cálculo seja a partir do número determinado pelo fatorial
 f = 1  # Fator nulo da multiplicação
 
-while count > 0:
-    print(count, end='')  # Printando o valor do contador enquanto é decrementado
-    print(' x ' if count > 1 else ' = ', end='')  # Printará 'x' até chegar no 1, pois após isso vem o '=' de resultado
-    f *= count  # Multiplicando o número com o próximo conforme é decrementado
+while count > 0:  # Enquanto o número não for decrementado até 0
+    print(count, end='')  # Fatorial sendo decrementado
+    print(' x ' if count > 1 else ' = ', end='')  # Printar multiplicação caso seja maior que 0, e igual caso termine.
+    f *= count
     count -= 1
 
 print(f'{f}')
--- Mundo 3/ex075.py
+++ Mundo 3/ex075.py
@@ -6,7 +6,6 @@ t1 = (int(input('Digite um  número: ')),
 print(f'O valor 9 apareceu {t1.count(9)} vezes.')
 print(f'O valor 3 está na {t1.index(3) + 1}ª posição') if 3 in t1 else print('O valor 3 não foi digitado em nenhuma '
                                                                              'posição')
-
 print(f'Os valores pares digitados foram:', end=' ')
 for c in t1:
     if c % 2 == 0:
