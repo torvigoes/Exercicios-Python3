1091cacf1a88777c9c474011fcc62b6986c120cb
Vitor Neves Góes <77070562+torvigoes@users.noreply.github.com>
2021-06-09 20:58:04 -0300

    up

--- Mundo 3/ex078.py
+++ Mundo 3/ex078.py
@@ -5,5 +5,12 @@ values = []
 for cont in range(5):
     values.append(int(input("Digite um valor: ")))
 
-print(f"\nO maior valor digitado foi: {max(values)}")
-print(f"O menor valor digitado foi: {min(values)}")
+maior = max(values)
+menor = min(values)
+
+for i, v in enumerate(values):  # I -> posição / V -> valor
+    if v == maior:
+        print(f'\nO maior número é {maior} e está na posição: {i+1}')
+for i, v in enumerate(values):  # I -> posição / V -> valor
+    if v == menor:
+        print(f"\nO menor número é {menor} e está na posição: {i+1}")
--- Mundo 3/ex081.py
+++ Mundo 3/ex081.py
@@ -4,10 +4,10 @@ count = 0
 resp = 'S'
 while resp == 'S':
     values.append(int(input("\nDigite um valor: ")))
-    count += 1
+    count += 1  # Contador para quantos números foram digidtados
     resp = input("Deseja continuar [S/N]: ").upper()
 
-values = sorted(values, reverse=True)
+values = sorted(values, reverse=True)  # Inverte a ordem da lista
 print(f"\nForam digitados {count} números.\n"
       f"Decrescente: {values}")
 
